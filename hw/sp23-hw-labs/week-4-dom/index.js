36940d2689442a2284cd9a212d433c7394555e58
jindacz <jindaz.work@outlook.com>
2023-04-28 19:54:26 -0700

    fin q4

--- hw/sp23-hw-labs/week-4-dom/index.js
+++ hw/sp23-hw-labs/week-4-dom/index.js
@@ -1,1 +1,25 @@
-// insert solution here
+const redBtn = document.querySelector('a[data-color="red"]');
+const yellowBtn = document.querySelector('a[data-color="yellow"]');
+const blueBtn = document.querySelector('a[data-color="blue"]');
+const menu = document.querySelector('.menu');
+
+redBtn.addEventListener('click', () => {
+  const div = document.createElement('div');
+  div.classList.add('section-color', 'red');
+  div.innerText = 'test';
+  menu.insertAdjacentElement('afterend', div);
+});
+
+yellowBtn.addEventListener('click', () => {
+  const div = document.createElement('div');
+  div.classList.add('section-color', 'yellow');
+  div.innerText = 'test';
+  menu.insertAdjacentElement('afterend', div);
+});
+
+blueBtn.addEventListener('click', () => {
+  const div = document.createElement('div');
+  div.classList.add('section-color', 'blue');
+  div.innerText = 'test';
+  menu.insertAdjacentElement('afterend', div);
+});
